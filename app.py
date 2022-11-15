dc0342d14a70b9d5ccd80587cdff8420839b92a5
lorettalw <102196131+lorettalw@users.noreply.github.com>
2022-11-15 13:53:38 -0500

    Changed field names, updated and added files for deployment

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 
@@ -14,35 +15,34 @@ ma = Marshmallow(app)
 
 class Amount(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), unique=False)
-    content = db.Column(db.Float, unique=False)
+    email = db.Column(db.String(100), unique=False)
+    amount = db.Column(db.String, unique=False)
 
-    def __init__(self, title, content):
-      self.title = title
-      self.content = content
+    def __init__(self, email, amount):
+      self.email = email
+      self.amount = amount
 
 class AmountSchema(ma.Schema):
     class Meta:
-        fields = ('title', 'content')
+        fields = ('email', 'amount')
 
 amount_schema = AmountSchema()
 amounts_schema = AmountSchema(many=True)
 
 
-
 @app.route('/amount', methods=["POST"])
 def add_amount():
-    title = request.json['title']
-    content = request.json['content']
+    email = request.json['email']
+    amount = request.json['amount']
 
-    new_amount = Amount(title, content)
+    new_amount = Amount(email, amount)
 
     db.session.add(new_amount)
     db.session.commit()
 
-    amount = Amount.query.get(new_amount.id)
+    damount = Amount.query.get(new_amount.id)
 
-    return amount_schema.jsonify(amount)
+    return amount_schema.jsonify(damount)
 
    
 @app.route('/amounts', methods=["GET"])
@@ -54,31 +54,32 @@ def get_amounts():
 
 @app.route("/amount/<id>", methods=["GET"])
 def get_amount(id):
-    amount = Amount.query.get(id)
-    return amount_schema.jsonify(amount)
+    damount = Amount.query.get(id)
+    return amount_schema.jsonify(damount)
 
 
 
 @app.route("/amount/<id>", methods=["PUT"])
 def amount_update(id):
-    amount = Amount.query.get(id)
-    title = request.json['title']
-    content = request.json['content']
+    damount = Amount.query.get(id)
+    email = request.json['email']
+    amount = request.json['amount']
 
-    amount.title = title
-    amount.content = content
+    damount.email = email
+    damount.amount = amount
 
     db.session.commit()
-    return amount_schema.jsonify(amount)   
+    return amount_schema.jsonify(damount)   
 
 
 @app.route("/amount/<id>", methods=["DELETE"])
 def amount_delete(id):
-    amount = Amount.query.get(id)
-    db.session.delete(amount)
+    damount = Amount.query.get(id)
+    db.session.delete(damount)
     db.session.commit()
 
-    return amount_schema.jsonify(amount)
+    return amount_schema.jsonify(damount)
+
 
 
 if __name__== '__main__':
